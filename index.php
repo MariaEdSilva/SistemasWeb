a31002b5a17ee40a781928314b88643fc6a5f772
alunoEtec <aula@etec.com>
2025-10-21 17:24:43 -0300

    criação do banco dadinhos<3

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Inicial</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/sidebar/sidebars.css" rel="stylesheet" />
+</head>
+<body>
+    <div class="container bg-dark h-100">
+        <div class="row">
+            <div class="col-3 p-2">
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <a href="instalar.php">
+                            Instalar o Banco de dados
+                        </a>
+                    </li>
+                    <li class="list-group-item">
+                        Listagem do banco
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="sidebars.js" class="astro-vvvwv3sm"></script>
+</body>
+</html>
